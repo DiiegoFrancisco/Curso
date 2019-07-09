1caeaacf28da1fd104ca2d3bfcd42e58fbb9bda2
DiiegoFrancisco <difrancisco2101@gmail.com>
2019-07-09 16:53:56 -0300

    Primeiro commit

---
+++ curso1/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  curso1
+//
+//  Created by DevMakerMobile on 09/07/2019.
+//  Copyright © 2019 DevMakerMobile. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var txIdade: UITextField!
+    @IBOutlet weak var lbResult: UILabel!
+    @IBAction func btDescobrir(_ sender: Any) {
+        
+        let idade = Int (txIdade.text!)! * 7
+         lbResult.text = "A idade do Cachorro é " + String (idade)
+        
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
